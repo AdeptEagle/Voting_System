953ab6450fb504f03754deb9102ffa2e95263e5b
AdeptEagle <kerbi2600@gmail.com>
2024-12-02 10:33:03 +0800

    [UPDATE] Added Voting to Menu

--- resources/views/Voting/candidates.blade.php
+++ resources/views/Voting/candidates.blade.php
@@ -7,61 +7,61 @@
   <link rel="stylesheet" href="{{asset('Css/candidates.css')}}"> 
 </head>
 <body>
-  <div class="sidebar hidden">
-    <div class="sidebar h2">
-        <nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="{{ route('Voting.Home') }}">
-                        <i class="fa fa-home fa-2x"></i>
-                        <span class="nav-text">
-                            Home
-                        </span>
-                    </a>
-                </li>
-                <li class="has-subnav">
-                    <a href="{{ route('Voting.Candidates') }}">
-                        <i class="fa fa-camera-retro fa-2x"></i>
-                        <span class="nav-text">
-                            SSC Candidates 
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="Voting.html">
-                        <i class="fa fa-box-archive fa-2x"></i>
-                        <span class="nav-text">
-                            Election Vote
-                        </span>
-                    </a>
-                </li>
-                <li class="has-subnav">
-                    <a href="Results.html">
-                        <i class="fa fa-chart-simple fa-2x"></i>
-                        <span class="nav-text">
-                            Election Results 
-                        </span>
-                    </a>
-                </li>
+<div class="sidebar hidden">
+        <div class="sidebar h2">
+            <nav class="main-menu">
+                <ul>
+                    <li>
+                        <a href="{{ route('Voting.Home') }}">
+                            <i class="fa fa-home fa-2x"></i>
+                            <span class="nav-text">
+                                Home
+                            </span>
+                        </a>
+                    </li>
+                    <li class="has-subnav">
+                        <a href="{{ route('Voting.Candidates') }}">
+                            <i class="fa fa-camera-retro fa-2x"></i>
+                            <span class="nav-text">
+                                SSC Candidates  
+                            </span>
+                        </a>
+                    </li>
+                    <li class="has-subnav">
+                        <a href="{{ route('voting.index') }}">
+                            <i class="fa fa-box-archive fa-2x"></i>
+                            <span class="nav-text">
+                                Election Voting
+                            </span>
+                        </a>
+                    </li>
+                    <li class="has-subnav">
+                        <a href="Results.html">
+                            <i class="fa fa-chart-simple fa-2x"></i>
+                            <span class="nav-text">
+                                Election Results 
+                            </span>
+                        </a>
+                    </li>  
+                </ul>
                 
-            </ul>
-            <ul class="logout">
+                <ul class="logout">
                     <li>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
                         <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                          <i class="fa fa-light fa-user fa-2x"></i>
+                        <i class="fa fa-light fa-user fa-2x"></i>
                             <span class="nav-text">
                                 Logout
                             </span>
                         </a>
                     </li>
-              </ul>
-        </nav>
+                </ul>
+            </nav>
+        </div>
     </div>
-</div>
-
+    
 <div class="scroll-container">
     <div class="card">
       <div class="flip-card-inner">
